b01b6badfcf3a62bb333ff8e205ccfb9e1a27666
Anthony Hamm <anthonyhamm31@hotmail.com>
2017-03-13 01:31:22 -0600

    AWS Prod changes
    Modify app.run() to be run in AWS

--- test.py
+++ test.py
@@ -496,4 +496,5 @@ def page_not_found(error):
 
 ## Main
 if __name__ == '__main__':
-    app.run(debug=True, port=8000, host='0.0.0.0')
+    #app.run(debug=True, port=8000, host='0.0.0.0')
+    app.run()
